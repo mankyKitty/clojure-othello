f6e44fcdd4ee5268d0ddfec57f0c191c1e16f46f
Sean Chalmers <sclhiannan@gmail.com>
2013-12-04 15:13:36 +0100

    Updated Clojure, Added Core.Match, Game Updates.
    Updated to Clojure 1.5.1, this should be awesome... in theory.
    
    Added core.match to simplify some functions, not much use yet but
    this will improve.
    
    Started really hacking on the game loop to actually make this sucker
    playable. Only the first move is working, the entire game dies when
    the second move is made. Not entirely sure why this is happening since
    the functions involved seem to checkout okay.. At least in inital
    testing.
    
    Probably should start hacking on a proper suite of tests for this to
    make sure I'm not doing something dense after the state of the game
    updates.
    
    Bleh...

--- src/othello/core.clj
+++ src/othello/core.clj
@@ -1,5 +1,6 @@
 (ns othello.core
-  (:use [clojure.string :only [join]])
+  (:use [clojure.string :only [join]]
+        [clojure.core.match :only (match)])
   (:gen-class))
 
 (def moves {:up         (fn [s max] (- s max))
@@ -22,17 +23,30 @@
   ;; placing the starting pieces.
   (* (- (/ n 2) 1) (+ n 1)))
 
+(defn switch-player [p]
+  ;; Give the opposite player to the one given.
+  (if (= p :black)
+    :white
+    :black))
+
+(defn printable-name [p]
+  ;; Give a screen friendly version of the current player.
+  (match p
+   :black "Black"
+   :white "White"))
+
 (defn calc-move [sq max func]
   ;; Given a starting place, board edge size and
   ;; direction - return the next square in that
   ;; direction.
   (let [new-sq ((func moves) sq max)]
-    (when (and (> new-sq 0) (< new-sq (* max max))) 
+    (when (and (> new-sq 0) (< new-sq (* max max)))
       {:dir func :sqr new-sq})))
 
 (defn get-moves [sq max]
   ;; From a given starting place, find the valid directions.
-  (filter #(not (nil? %)) (map #(calc-move sq max %) (keys moves))))
+  ;; (filter #(not (nil? %)) (map #(calc-move sq max %) (keys moves)))
+  (filter identity (map (partial calc-move sq max) (keys moves))))
 
 (defn starting-places [n]
   ;; Get the locations of our starting pieces
@@ -92,6 +106,10 @@
   ;; Is the square empty?
   (= :empty (:status sqr)))
 
+(defn opp-player-sqr? [player sqr board]
+  ;; Check if a square belongs to the other player
+  (not= player (:status (nth board sqr))))
+
 (defn filter-tiles-by-not-status [pred coll board]
   ;; Given a square status, a list of squares and a current play
   ;; board, filter the list of squares to ones that match the
@@ -121,7 +139,7 @@
   ;; not take into account the status of the tiles. Don't be greedy :(
   (take-while identity (iterate #(:sqr (calc-move % max direction)) start)))
 
-(defn squares-to-flip [player row col-char max board]
+(defn valid-move-directions [player row col-char max board]
   ;; Take player input and get a list of the valid directions that the
   ;; player is allowed to move.
   (let [target (convert-move-input row col-char max)
@@ -153,9 +171,52 @@
       (print (str ((:status sqr) square-states) " | ")))
     (print "\n")))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+(defn get-flippable-tiles [board max player start]
+  ;; From a given starting square, create a list of the tiles that are
+  ;; fippable.
+  (for [{:keys [status sqr] :as original} (map #(nth board %) (move-sequence (:sqr start) (:dir start) max))
+         :while (not= player status)]
+     (hash-map original {:status (switch-player status) :sqr sqr})))
+
+(defn squares-to-flip [starting-places board max player]
+  ;; Get a list of the squares that have tiles we want to flip.
+  (let [[fst & rest] (flatten (map #(get-flippable-tiles board max player %) starting-places))]
+    (merge fst rest)))
+
+(defn process-input [s]
+  ;; Take the read-line input and get something we can try to use as
+  ;; valid move input.
+  (let [[row col] (filter #(not= "" %) (clojure.string/split s #""))]
+    [row col]))
+
+(defn -main [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (let [max 8
+        board (make-board max)]
+    (loop [game-board board
+           player :black
+           turn 0]
+      ;; Display the latest board.
+      (print-board game-board max)
+      ;; Ask the current player for their desired move.
+      (println (str (printable-name player) " Player's Turn!"))
+      (println "Enter Row and Column Number (eg. 3d):")
+      ;; Try to do something with it all.
+      ;; If we have a parse-able input that isn't a quit instruction
+      ;; and that move produces a valid change on the board THEN we want to
+      ;; actually progress the game forward.
+      (let [[row col] (process-input (read-line))
+            sqr (valid-move-directions player (Integer/parseInt row) col max game-board)
+            placement (convert-move-input (Integer/parseInt row) col max)
+            flip (squares-to-flip sqr game-board max player)
+            all-moves (merge flip {{:status :empty :sqr placement} {:status player :sqr placement}})]
+        ;; Assuming we have a valid placement, try to make the move(s).
+        (println all-moves)
+        (println flip)
+        (when (< turn 5)
+          ;; Before looping the board I have to replace any changing squares that
+          ;; have resulted from any new moves. The result of a move is
+          (recur (replace all-moves game-board) (switch-player player) (inc turn))))
+      ;; Check victory conditions and then move on to the next player turns.
+      )))
